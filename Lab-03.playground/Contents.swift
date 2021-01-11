117617facd46ff751eae587f851d043bdc008f88
Mitchell <soggybag@gmail.com>
2021-01-11 14:43:00 -0800

    updated

--- Lab-03.playground/Contents.swift
+++ Lab-03.playground/Contents.swift
@@ -28,30 +28,73 @@ func returnsSomething(name: String) -> String {
     return "Hello \(name)!" // returns a string
 }
 
-// Quick note! Concatenate a variable with a string using \(). Take a look at the
-// funftion above. Notice the variable `name` is
+var something = returnsSomething(name: "fancy")
+// Here `something` is assigned a string and is type string since the
+// function Returns a string.
 
-// Add a comment on line 22, what happens? You should get an error:
-// Missing return in a function expected to return 'String'
 
-// When calling a function you'll use the name along with the value:
+// This means that if you had a variable that was type string:
+var somethingElse: String
+// You could use `returnSomething` to assign a value:
+somethingElse = returnsSomething(name: "Fantastic!")
+// But if you had a variable that was type Int you would get an error
+var otherStuff: Int
+// The line below shows an error: Cannot assign value of type 'String' to type 'Int'
+//otherStuff = returnsSomething(name: "Boondoggle!")
 
-returnsSomething(name: "Mungo") // notice we included the name!
+
+
+
+
+
+// Quick note! Concatenate a variable with a string using \().
+var twentyTwo = 22
+// Combine the variable above with the string below
+var description = "There are \(twentyTwo) reasons..."
+print(description)
+// You can put any valid expression in the \(...)
+print("Todays greeting is: \(returnsSomething(name: "Fungi"))")
+print("The number Pi is roughly \(22.0/7.0)")
+
+
+
+
 
 // Challenge:
 // Write the following functions:
 
-// 1. Write a function that calculates the area of a rectangle.
+// 1. Write a function that prints your name using print().
+
+
+
+// 2. Call/invoke the function you just wrote
+
+
+
+// 3. Write a function that takes a String as a parameter.
+//   Your function should print the string with "***" before and
+//   after the input string.
+
+
+
+// 4. Write a function that calculates the area of a rectangle.
 //   It should take the width and height as parameters of type Int
 //   and return an Int
 
 
-// 2. Write a function that creates generates a greeting. It should
-//   take name a string as a parameter and return a string.
+
+// 5. Call your area function with the values 25 and 35 and print
+//   returned results.
 
 
-// 3. Write a function that calculates the area of a circle. It
-//   take radius, a Double, and return a Double.
+
+// 6. Write a function that calculates the area of a circle. It
+//   take radius, an Int, and return a Double. The area of a
+//   is roughly 3.14 * radius * radius.
+//   You'll have to convert the radius to Double to do the math!
+
+
+
 
 
 
@@ -70,6 +113,15 @@ func someFunc(externalName internalName: Int) -> Int {
 
 let res = someFunc(externalName: 40) // externalName is used here!
 
+// Read the two code blocks above carefully. Notice in the first the
+// variable used inside the function is `internalName` and in the second
+// the name for the argument is `externalName`
+
+
+
+
+
+
 // The function below uses only the internal names. This means you'll
 // need to use the names: width1, height1, width2, and heigh2
 
@@ -80,22 +132,32 @@ func resize(width1: Int, height1: Int, width2: Int, height2: Int) {
 // Use width1, height2, width2, height2 here also
 resize(width1: 20, height1: 30, width2: 100, height2: 400)
 
-// adding some external and internals names you could write this
+// Notice here the external names are the same as the internal name.
+
+
+
+
+// Adding some external and internals names you could rewrite this
 // function like this:
 
 func resize2(fromWidth width1: Int, fromHeight height1: Int, toWidth width2: Int, toHeight height2: Int) {
-    // Does stuff with width1, height1, width2, height2
+    print("Previous dimensions: \(width1) x \(height1)")
     // ...
+    print("New dimensions: \(width2) x \(height2)")
 }
 
 // Invoking the function you'll use the names: fromWidth, fromHeight, toWidth, toHeight
 resize2(fromWidth: 20, fromHeight: 30, toWidth: 100, toHeight: 400)
 
 
-// Why use internal and external names? Naming is important. The names used
-// for parameters can have a different semantic meaning when calling a function
+
+// Why use internal and external names?
+
+// Naming is important. The names used for parameters can have a
+// different semantic meaning when calling a function
 // and when used inside a function. Using different names in each place can help
-// clarify how to use a function.
+// clarify how to use a function. Expect to see this often when programming
+// in Swift and UIKit it's how they do it!
 
 // Consider this:
 
@@ -109,25 +171,28 @@ greet(user: "Frango", withPrefix: "Mr.")
 // This reads as: greet user with prefix which maybe makes it more clear.
 
 
+
 // Write some new functions. Be sure to make use of internal and extrernal paramters.
 
-// 1. Imagine you are making app to help contractors calculate area.
-//   This would help them estimate cost for materials and time. Your
-//   first function will calculate the area of a square. Your function
-//   should accept two Int values: height and width, and return area as
-//   an Int. Remember, area is calculated as height multiplied by width.
+// 1. Imagine you are making an app to help contractors calculate area.
+//    This would help them estimate cost for materials and time. Your
+//    first function will calculate the area of a square. Your function
+//    should accept two Int values: height and width, and return area as
+//    an Int. Remember, area is calculated as height multiplied by width.
+
 
 
 // 2. Now that you have area you might also need to know the perimeter.
-//   Contractors would use this to calculate how much molding or framing
-//   material might be needed. Remember, perimeter is calculated as the
-//   sum of all sides. Takes 4 parameters all int and returns an int.
+//    Contractors would use this to calculate how much molding or framing
+//    material might be needed. Remember, perimeter is calculated as the
+//    sum of all sides. Takes 4 parameters all int and returns an int.
+
 
 
 // 3. Write a function that orders a pizza. It should take type of
-//   pie as a string, the size of the pie inches as an int, and return
-//   cost of the pie as a double. Calculate the cost as the area
-//   times 0.05
+//    pie as a string, the size of the pie inches as an int, the
+//    number of toppings as an Int, and return cost of the pie as a double.
+//    Calculate the cost as the area times 0.05 + 0.03 * number of toppings
 
 
 

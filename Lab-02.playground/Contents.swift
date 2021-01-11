6371ad6202783b262a35aac4e07ad4ec9b618abe
Mitchell <soggybag@gmail.com>
2021-01-11 14:57:43 -0800

    updated again

--- Lab-02.playground/Contents.swift
+++ Lab-02.playground/Contents.swift
@@ -7,7 +7,17 @@ import UIKit
 let myTuple = (12, 23)
 
 // You can mix types in a tuple:
-let otherTup = ("Name", 9.99)
+var otherTup = ("Name", 9.99)
+
+// But the types always have to match:
+
+otherTup = ("Bean Soup", 3.78)
+
+//otherTup = (5.66, "What!")
+// Throws error:
+// Cannot assign value of type '(Double, String)' to type (String, Double)
+
+
 
 // Tuples can have more than two values:
 let truple = (1,2,3)
@@ -15,34 +25,44 @@ let qruple = (1,2,3,4)
 
 // Option click on each of the variable above to
 // check it's type.
-myTuple // (Int, Int)
+myTuple  // (Int, Int)
 otherTup // (String, Int)
-truple // (Int, Int, Int)
-qruple // (Int, Int, Int, Int)
+truple   // (Int, Int, Int)
+qruple   // (Int, Int, Int, Int)
+
 
 // Challenge:
 // Define the following tuples:
 
-// 1. login holds name and password
-
-// 2. vector holds two nunmbers: 1.56, 3.45
-
-// 3. profile holds name, age, shoesize
-
-// 3. address holds street address, city, state, and zip code
+// 1. Login holds name and password both type String
 
 
 
+// 2. Vector holds two nunmbers: 1.56, 3.45
 
 
 
-// You can define a tuple as a type:
+// 3. Profile holds name, age, shoesize
+
+
+
+// 3. Address holds street address, city, state, and zip code
+
+
+
+
+
+
+// Above the types were Implicit. Tuple types can be
+// expressed explicitly. For example:
+
 let coords: (Double, Double) = (37.754871, -122.498789)
 
+
 // Challenge:
-// While Swift will implicitly type these at times it good
+// While Swift will implicitly type these it is good
 // practice to also add the type to prevent mistakes.
-// Write the typesd for the following tuples:
+// Explicitly type each of the variables below:
 
 let name = ("Frango", "Martini")
 let pizza = ("Peperoni", "Large", 21.99)
@@ -51,12 +71,13 @@ let damage = ("heat", 120000)
 
 
 // Accessinng the values of a tuple is similar to accessing values
-// from an array
+// from an array, object or dictionary and a little different.
+// Each value is indexed. Use the dot (.) followed by the index.
 
 print(name.0) // Frango
 print(name.1) // Martini
 
-// Challenge
+// Challenge:
 // Print all of the values from the pizza and damage
 
 
@@ -72,18 +93,25 @@ let track: (name: String, time: Double) = ("Happy Birthday", 1.57)
 print(track.name) // Happy Birthday
 print(track.time) // 1.57
 
-// Challenge
-// Define tuples for the following, be sure to include names for each value:
+
+// Challenge:
+// Define tuples for the following, be sure to include names for
+// each value. Also assign a valid value for each variable.
 
 // 1. location: latitude: 37.754871, longitude: -122.498789
 
+
+
 // 2. doughnut: type: cake, flavor: chocolate, has sprinkles: false
+
+
 
 // 3. shoe: model: Skate, color: red, size: 9.5
 
 
-// Challenge
-// Print each valeu from the three tuples you defined above:
+
+// Challenge:
+// Print each value from the three tuples you defined above:
 
 
 
